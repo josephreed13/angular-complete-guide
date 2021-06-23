7884ed3ea466983c13fbaa331ebb431a46fb3f6e
Joseph Reed <reed.joseph13@gmail.com>
2021-06-23 14:20:38 -0500

    assignment 2 - databinding

---
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SolutionComponent } from './solution/solution.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
